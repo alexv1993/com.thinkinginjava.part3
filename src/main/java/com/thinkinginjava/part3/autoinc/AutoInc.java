e8b1cc2e9af51bc0b1af191953a47fa3e8e0ba64
alexv1993 <alexvendin@gmail.com>
2017-10-08 23:53:23 +0300

    added examples of using an increment operator

---
+++ src/main/java/com/thinkinginjava/part3/autoinc/AutoInc.java
@@ -0,0 +1,17 @@
+package com.thinkinginjava.part3.autoinc;
+
+/**
+ * Created by Alex on 08.10.2017.
+ */
+public class AutoInc {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("i: " + i);
+        System.out.println("++i: " + ++i);
+        System.out.println("i++: " + i++);
+        System.out.println("i: " + i);
+        System.out.println("--i: " + --i);
+        System.out.println("i--: " + i--);
+        System.out.println("i: " + i);
+    }
+}
