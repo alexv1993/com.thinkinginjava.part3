651b5d9b690383e49d65d4ff441ed7f34558b855
alexv1993 <alexvendin@gmail.com>
2017-10-08 22:42:28 +0300

    sources

---
+++ src/main/java/com/thinkinginjava/part3/Assignment2.java
@@ -0,0 +1,27 @@
+package com.thinkinginjava.part3;
+
+/**
+ * Created by Alex on 08.10.2017.
+ */
+public class Assignment2 {
+    public static void main(String[] args) {
+        Art art1 = new Art();
+        Art art2 = new Art();
+
+        art1.distance = 2000;
+        art2.distance = 2500;
+
+        System.out.println("1: art1.distance: " + art1.distance + " art2.distance: " + art2.distance);
+
+        art1 = art2;
+
+        System.out.println("2: art1.distance: " + art1.distance + " art2.distance: " + art2.distance);
+
+        art1.distance = 3000;
+
+        System.out.println("3: art1.distance: " + art1.distance + " art2.distance: " + art2.distance);
+
+
+    }
+
+}
---
+++ src/main/java/com/thinkinginjava/part3/Tank.java
@@ -0,0 +1,8 @@
+package com.thinkinginjava.part3;
+
+/**
+ * Created by Alex on 08.10.2017.
+ */
+public class Tank {
+    int level;
+}
